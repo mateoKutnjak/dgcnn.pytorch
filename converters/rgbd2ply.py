c0c8a61e90877fae9d35f7197ea1bd11428c8e10
kutnjaci <mateo.kutnjak@gideonbros.ai>
2020-03-16 22:49:39 +0100

    depth/rgbd -> ply converter

--- converters/rgbd2ply.py
+++ converters/rgbd2ply.py
@@ -14,26 +14,30 @@ def generate_pointcloud(args):
     Parameters
     args (argparse.Namespace): cmd line arguments for pointcloud generation 
     """
-    rgb = cv2.imread(args.rgb)
-    rgb = cv2.cvtColor(rgb, cv2.COLOR_BGR2RGB)
+    if args.rgb is not None:
+        rgb = cv2.imread(args.rgb)
+        rgb = cv2.cvtColor(rgb, cv2.COLOR_BGR2RGB)
     depth = cv2.imread(args.depth, cv2.IMREAD_ANYCOLOR | cv2.IMREAD_ANYDEPTH)[:, :, 0]
 
-    if rgb.shape[:2] != depth.shape:
+    if args.rgb is not None and (rgb.shape[:2] != depth.shape):
         raise Exception("Color and depth image do not have the same resolution.")
 
     points = []    
-    for v in range(rgb.shape[1]):
-        for u in range(rgb.shape[0]):
-            color = rgb[u, v]
-            Z = depth[u, v] / args.scaling_factor
-
-            if Z == 0: continue
+    for v in range(depth.shape[1]):
+        for u in range(depth.shape[0]):
+            if depth[u, v] == 0:
+                continue
             
+            Z = depth[u, v] / args.scaling_factor
             X = (u - args.cx) * Z / args.fx
             Y = (v - args.cy) * Z / args.fy
             
-            points.append("%f %f %f %d %d %d 0\n"%(X, Y, Z, color[0], color[1], color[2]))
-    
+            if args.rgb is not None:
+                color = rgb[u, v]
+                points.append("%f %f %f %d %d %d 0\n"%(X, Y, Z, color[0], color[1], color[2]))
+            else:
+                points.append("%f %f %f 0\n"%(X, Y, Z))
+
     file = open(args.output, "w")
     file.write('''ply
 format ascii 1.0
@@ -41,13 +45,14 @@ element vertex %d
 property float x
 property float y
 property float z
-property uchar red
-property uchar green
-property uchar blue
+%s
 property uchar alpha
 end_header
 %s
-'''%(len(points),"".join(points)))
+'''%(len(points), '''property uchar red
+property uchar green
+property uchar blue
+''' if args.rgb else '', "".join(points)))
     file.close()
 
 
@@ -55,14 +60,13 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     
     parser.add_argument('--rgb', '-r', help='input RGB image (format: .png)')
-    parser.add_argument('--depth', '-d', help='input depth image (format: .exr)')
-    parser.add_argument('--output', '-o', help='output pointcloud file (format: .ply)')
-    parser.add_argument('--fx', type=float, help='focal length x')
-    parser.add_argument('--fy', type=float, help='focal length y')
-    parser.add_argument('--cx', type=float, help='principal point x')
-    parser.add_argument('--cy', type=float, help='principal point y')
-    parser.add_argument('--scaling-factor', type=float, help='scaling factor')
-    # TODO put camera intrinsics to config file
+    parser.add_argument('--depth', '-d', required=True, help='input depth image (format: .exr)')
+    parser.add_argument('--output', '-o', required=True, help='output pointcloud file (format: .ply)')
+    parser.add_argument('--fx', type=float, required=True, help='focal length x')
+    parser.add_argument('--fy', type=float, required=True, help='focal length y')
+    parser.add_argument('--cx', type=float, required=True, help='principal point x')
+    parser.add_argument('--cy', type=float, required=True, help='principal point y')
+    parser.add_argument('--scaling-factor', type=float, default=1000.0, help='scaling factor')
 
     args = parser.parse_args()
 
